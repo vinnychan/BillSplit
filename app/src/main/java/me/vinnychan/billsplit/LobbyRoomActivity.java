c7f13e7c7bf5a5c3c4d86edac01157f964d8600d
Vincent Chan <v.chan36@gmail.com>
2015-10-18 01:00:57 -0700

    firebase initialization

--- app/src/main/java/me/vinnychan/billsplit/LobbyRoomActivity.java
+++ app/src/main/java/me/vinnychan/billsplit/LobbyRoomActivity.java
@@ -6,12 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class LobbyRoomActivity extends AppCompatActivity {
 
     String username;
+
+    private Firebase fireBaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,30 @@ public class LobbyRoomActivity extends AppCompatActivity {
 
         username = getIntent().getStringExtra("USERNAME");
 
+        Firebase.setAndroidContext(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final TextView mTextCondition = (TextView) findViewById(R.id.mTextCondition);
+
+        fireBaseRef = new Firebase("https://billsplitdubhacks.firebaseio.com");
+
+        fireBaseRef.child("Item 1").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getValue());
+                mTextCondition.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 
 }
