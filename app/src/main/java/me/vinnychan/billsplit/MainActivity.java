bfa64e54335a56b5790e2f5f012c6d30a3cbadc1
Vincent Chan <v.chan36@gmail.com>
2015-10-17 22:00:39 -0700

    Initial activities and layout

--- app/src/main/java/me/vinnychan/billsplit/MainActivity.java
+++ app/src/main/java/me/vinnychan/billsplit/MainActivity.java
@@ -1,7 +1,12 @@
 package me.vinnychan.billsplit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button joinButton = (Button) findViewById(R.id.join_button);
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText usernameEditText = (EditText) findViewById(R.id.username_edittext);
+                String username = usernameEditText.getText().toString();
+
+                if (username.equals("")) {
+                    return;
+                }
+
+                Toast.makeText(getBaseContext(), "Welcome " + username + "!", Toast.LENGTH_LONG).show();
+
+                // To change to a new activity, we do it with "Intents"
+                Intent goToChatRoom = new Intent(getBaseContext(), LobbyRoomActivity.class);
+                goToChatRoom.putExtra("USERNAME", username);
+                startActivity(goToChatRoom);
+
+            }
+        });
     }
 }
