979d5b0669e0409ea76cfb0fb6015b830a2ee7f8
Ting Ting Tai <tting.git@gmail.com>
2015-10-18 01:08:41 -0700

    model added getter and setters (not fully implemented yet)

--- app/src/main/java/me/vinnychan/billsplit/model/User.java
+++ app/src/main/java/me/vinnychan/billsplit/model/User.java
@@ -4,10 +4,17 @@ package me.vinnychan.billsplit.model;
  * Created by TING on 17-Oct-2015.
  */
 public class User {
+    private String id;
     private String name;
 
     public User(String name) {
+        id = ""; //todo
         this.name = name;
     }
 
+    public String getID() { return id; }
+
+    public String getName() { return name; }
+
+    public void setName(String nm) { name = nm; }
 }
